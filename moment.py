187a7cb890e03df2e97991d7270e888cb4dd8eae
Joseph Rudick <69953227+jdrinoca@users.noreply.github.com>
2020-08-31 12:47:23 -0700

    Update moment.py

--- moment.py
+++ moment.py
@@ -1,6 +1,9 @@
 
 # moment instance
-#
+
+# Joseph Rudick
+# Edited: 8/31/2020
+
 # node - connected node (node instance)
 # vector - moment vector (float, can be negative or positive)
 # color - moment color (matplotlib color string)
